38a87a876e103a8f06d52c21c21ee54af3d017f1
Valerii <valerii.savchenko@icloud.com>
2020-10-15 12:24:42 +0300

    Initial commit; Added IUnitOfWork interface

---
+++ IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using CabaVS.Shared.Abstractions.Repository;
+using System.Threading.Tasks;
+
+namespace CabaVS.Shared.Abstractions.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+
+        Task<int> CommitAsync();
+    }
+}
